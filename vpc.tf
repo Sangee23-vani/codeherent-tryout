8ec5b56366ec5ab4e556c73f56e16d9f7975f52a
Sangeethavanit <sangeethat@teezle.com>
2020-12-23 16:12:21 +0530

    vpc file added

---
+++ vpc.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Mini Project"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Mini Project"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Mini Project"
+  }
+}
