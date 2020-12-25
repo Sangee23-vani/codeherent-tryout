0270cc8ae0a7c3f074d9b9fd97846090d05b00d0
Sangeethavanit <sangeethat@teezle.com>
2020-12-25 13:21:36 +0530

    vpc created

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
-output "instance_ip_addr" {
-  value = aws_instance.my_instance.*.private_ip
-}
-
-resource "aws_route_table" "route_table1" {
-  vpc_id = 
-}
-
-resource "aws_main_route_table_association" "main_route_table_association" {
-  vpc_id = 
-  route_table_id = 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "Mini Project"
+  }
 }
